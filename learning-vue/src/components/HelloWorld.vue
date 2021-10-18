6b74c5d3b0971dfb544aa73e45a73d55992024a5
nazarzadehjazmin <nazarzadehjazmin@gmail.com>
2021-10-18 15:46:04 -0300

    components

---
+++ learning-vue/src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>{{ text }}</h2>
+    <MyComponent></MyComponent>
+  </div>
+</template>
+
+
+<script>
+/*For adding another component to this file*/
+import MyComponent from './MyComponent.vue';
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    MyComponent
+  },
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      text: "Hello world! I'm from components"
+    }
+  }
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ learning-vue/src/components/MyComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="my-component">
+      <hr/>
+    <h1>This is My Component</h1>
+    <h2>{{ subtitle }}</h2>
+    <hr/>
+  </div>
+</template>
+
+<script>
+export default{
+    name: 'MyComponent',
+    data() {
+        return {
+            subtitle: 'Subtitle from My Component'
+        }
+    }
+};
+</script>
